c3f2fea71edac4c18fa78536a8ae7a08b93c04f8
GuruGroup8 <darelee@naver.com>
2022-02-25 23:44:10 +0900

    Roby
    sss

---
+++ MJgame/Assets/Script/AppearRobyImage.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AppearRobyImage : MonoBehaviour
+{
+    Vector2 dir;//Vector2: float 데이터형 x,y 멤버변수
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
