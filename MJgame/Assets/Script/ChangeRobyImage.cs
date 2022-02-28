6bf235812ef732e79d0d14a2b5166318346b7d99
GuruGroup8 <darelee@naver.com>
2022-02-28 22:13:50 +0900

    dd
    dd

--- MJgame/Assets/Script/ChangeRobyImage.cs
+++ MJgame/Assets/Script/ChangeRobyImage.cs
@@ -1,19 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ChangeRobyImage : MonoBehaviour
 {
-
+    public Sprite Hanul;
+    public Sprite Juwon;
+    Image thisImg;
     // Start is called before the first frame update
     void Start()
     {
-        
+        thisImg = GetComponent<Image>();
     }
 
     // Update is called once per frame
-    void Update()
+    void ChangeImg()
     {
-        
+        if(thisImg.sprite = Juwon)
+        {
+            thisImg.sprite = Hanul;
+        }
     }
 }
